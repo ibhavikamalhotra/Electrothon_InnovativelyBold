7e80c7947910b34d70077be7c2b816f35ba2f981
Harshit Sharma <iharshitsharma@outlook.com>
2022-02-20 21:58:37 +0530

    API Endpoint update
    fix: api get req. blocked due to CORS policy, changed endpoint

--- dash.js
+++ dash.js
@@ -1,19 +1,30 @@
-const url =
-  "https://api.covid19india.org/data.json";
+const url = "https://data.covid19india.org/data.json";  
 fetch(url)
-.then((resp) => resp.json()) // Transform the data into json
-.then(function(data) {
-    
-    console.log(data.statewise[0])
-    document.getElementById("deaths").innerHTML = data.statewise[0].deaths;
-    document.getElementById("deathsnew").innerHTML = data.statewise[0].deltadeaths;
-    document.getElementById("total").innerHTML = data.statewise[0].confirmed;
-    document.getElementById("newcases").innerHTML = data.statewise[0].deltaconfirmed;
-    document.getElementById("activecases").innerHTML = data.statewise[0].active;
-    document.getElementById("recovered").innerHTML = data.statewise[0].recovered;
-
+.then((response) => {
+    if (!response.ok) {
+        throw new Error(response.error)
+    }
+    return response.json();
 })
-  .catch(function (err) {
-    // There was an error
-    console.warn("Something went wrong.", err);
-  });
+.then(data => {
+  console.log(data.statewise[0])
+      document.getElementById("deaths").innerHTML = data.statewise[0].deaths;
+      document.getElementById("deathsnew").innerHTML = data.statewise[0].deltadeaths;
+      document.getElementById("total").innerHTML = data.statewise[0].confirmed;
+      document.getElementById("newcases").innerHTML = data.statewise[0].deltaconfirmed;
+      document.getElementById("activecases").innerHTML = data.statewise[0].active;
+      document.getElementById("recovered").innerHTML = data.statewise[0].recovered;
+  
+})
+.catch(function(error) {
+  console.log(error);
+});
+// , {
+//   method : "GET",
+//   mode: 'no-cors',
+//   headers:{
+//     "Access-Control-Allow-Origin":"*",
+//     "Access-Control-Allow-Methods": "GET, OPTIONS, POST, PUT",
+//     "Access-Control-Allow-Headers": "X-Token"
+//   }    
+// }
